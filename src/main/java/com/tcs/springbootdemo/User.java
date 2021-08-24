dde3c3189dac12ae24223a469c310a77dd293909
tcsrutvi <tcsrutvi@gmail.com>
2021-08-24 12:43:56 +0530

    first commit

---
+++ src/main/java/com/tcs/springbootdemo/User.java
@@ -0,0 +1,16 @@
+package com.tcs.springbootdemo;
+
+public class User {
+
+	private String firstName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+}
