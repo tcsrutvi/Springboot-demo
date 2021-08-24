aa0450e4b80bb06e2564220985272be692eca5ee
tcsrutvi <tcsrutvi@gmail.com>
2021-08-24 12:55:28 +0530

    layer 2

--- src/main/java/com/tcs/springbootdemo/UserController.java
+++ src/main/java/com/tcs/springbootdemo/UserController.java
@@ -1,5 +1,6 @@
 package com.tcs.springbootdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+	@Autowired //DI
+	IUserService userService;
+	
 	@GetMapping("/getuser")
 	private String getUser() {
 		System.out.println("called");
@@ -15,6 +19,7 @@ public class UserController {
 	}
 	@PostMapping("/user")
 	private void saveUser(@RequestBody User user) {
+		userService.save(user);
 		System.out.println(user.getFirstName());
 	}
 }
